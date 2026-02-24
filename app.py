83c1d88e489c94ad5af3994f0709d3d006792580
archita9131 <architapeddamushti007@gmail.com>
2026-02-24 19:58:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify, render_template,session
 import mysql.connector
 from datetime import datetime
+import os
 
 app = Flask(__name__)
 app.secret_key = "library_secret_key"
@@ -389,5 +390,9 @@ def all_returns():
     return jsonify(data)
 
 # ---------------- RUN ----------------
+
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
+
